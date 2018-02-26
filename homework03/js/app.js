3c543ef648063573f77ae5aec62e650a6ec9a475
Yevhen Kondratyev <kondratyev.yevhen@gmail.com>
2018-02-26 10:36:29 +0200

    homework04

--- homework03/js/app.js
+++ homework03/js/app.js
@@ -1,12 +1,33 @@
-"use strict";
+'use strict';
 
 function ready() {
-    let headerMenuButton = document.querySelector(".nav-btn-container");
+    let headerMenuButton = document.querySelector('.nav-btn-container');
+    let geocoder = new google.maps.Geocoder();
+    let address = 'вулиця Академіка Підстригача, Львів, Львівська область, 79000';
 
     function toggleButton() {
-        headerMenuButton.classList.toggle("close");
+        headerMenuButton.classList.toggle('close');
+        headerMenuButton.removeEventListener('click', toggleButton, false);
+        setTimeout(function(){
+            headerMenuButton.addEventListener('click', toggleButton, false);
+        },300);
     }
-    headerMenuButton.addEventListener("click", toggleButton, false);
+    headerMenuButton.addEventListener('click', toggleButton, false);
+
+    geocoder.geocode({'address': address}, function (results, status) {
+        let coord = {
+            lat: Number.parseFloat(results[0].geometry.location.lat()),
+            lng: Number.parseFloat(results[0].geometry.location.lng())
+        };
+        let map = new google.maps.Map(document.querySelector('.footer-map'), {
+            zoom: 15,
+            center: coord
+        });
+        let marker = new google.maps.Marker({
+            position: coord,
+            map: map
+        });
+    });
 }
 
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener('DOMContentLoaded', ready);
