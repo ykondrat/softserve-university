d370f9f69f13936131b5e27c7cf8341f98b69428
Yevhen Kondratyev <kondratyev.yevhen@gmail.com>
2018-03-02 18:00:43 +0200

    homework

--- homework06/js/index.js
+++ homework06/js/index.js
@@ -1,28 +1,73 @@
-function fibonacci(index) {
-    if(index < 0) {
-        return (0);
-    } else if (index <= 1) {
-        return (index);
-    } else {
-        return (fibonacci(index - 1) + fibonacci(index - 2));
+'use strict';
+
+const resultBlock = document.querySelector('.result');
+const button = document.querySelector('.submit');
+const select =document.querySelector('.method');
+let method = 'fibonacciRecursive';
+
+function removeChildFromResult() {
+    while (resultBlock.firstChild) {
+        resultBlock.removeChild(resultBlock.firstChild);
     }
 }
 
-let button = document.querySelector('.submit');
+function fibonacciIterative(index) {
+    let currentResult = 1;
+    let prevResult = 1;
+    let temp;
 
-button.addEventListener('click', function() {
-    let index = parseInt(document.querySelector('.index').value);
-    let arr = new Array();
-    let resultBlock = document.querySelector('.result');
+    for (let i = 0; i < index - 1; i++) {
+		temp = currentResult;
+		currentResult = currentResult + prevResult;
+		prevResult = temp;
+	}
+	return (currentResult);
+}
 
-    for (let i = 0; i < index; i++) {
-        arr.push(fibonacci(i));
-    }
+function fibonacciRecursive(index) {
+    return (index <= 1 ? 1 : (fibonacciRecursive(index - 1) + fibonacciRecursive(index - 2)));
+}
 
-    arr.forEach(function(element){
+function showResult(arrOfResult) {
+    arrOfResult.forEach(function(element, index) {
         let span = document.createElement('span');
-        let textNode = document.createTextNode(`${element}, `);
+        let textNode;
+
+        textNode = index !== arrOfResult.length - 1 ? document.createTextNode(`${element}, `) : document.createTextNode(`${element}`);
         span.appendChild(textNode);
         resultBlock.appendChild(span);
     });
-}, false);
+}
+
+function showError() {
+    let span = document.createElement('span');
+    let textNode = document.createTextNode('Bad index');
+
+    span.appendChild(textNode);
+    resultBlock.appendChild(span);
+}
+
+select.addEventListener('change', function() {
+    method = this.value;
+});
+
+button.addEventListener('click', function() {
+    const index = parseInt(document.querySelector('.index').value);
+    let arr = new Array();
+
+    removeChildFromResult();
+    if (index >= 0 && !isNaN(index)) {
+        if (method === 'fibonacciRecursive') {
+            for (let i = 0; i < index; i++) {
+                arr.push(fibonacciRecursive(i));
+            }
+        } else {
+            for (let i = 0; i < index; i++) {
+                arr.push(fibonacciIterative(i));
+            }
+        }
+        showResult(arr);
+    } else {
+        showError();
+    }
+});
